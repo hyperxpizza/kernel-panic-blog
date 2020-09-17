cba103bcd91148d94b49c8c17df43f53336c6e07
hyperxpizza <wojtek.frackowski@gmail.com>
2020-09-17 03:59:59 +0200

    finnally forward with blog client

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
-
-//bullshit post import 
-import Post from './Components/Post';
-
+import React, { useEffect, useState } from 'react';
+import PostList from './Components/PostList';
+import PostListLoading from './Components/PostListLoading';
 function App() {
+  const ListLoading = PostListLoading(PostList);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `http://localhost:8888/posts`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((repos) => {
+        setAppState({ loading: false, repos: repos });
+      });
+  }, [setAppState]);
   return (
-    <div className="App">
-      <Post />
+    <div className='App'>
+      <div className='container'>
+        <h1>My Posts</h1>
+      </div>
+      <div className='repo-container'>
+        <PostListLoading isLoading={appState.loading} repos={appState.repos} />
+      </div>
     </div>
   );
 }
-
 export default App;
--- blog/src/Components/Post.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Post(props) {
-    return (
-        <h1> this is a post </h1>
-    );
-}
-
-export default Post;
