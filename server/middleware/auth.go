ff5dbef174fdcb1ed83e9ef8988ac887f24a5ad0
hyperxpizza <wojtek.frackowski@gmail.com>
2020-10-03 09:03:25 +0200

    token valid 1hour

--- server/middleware/auth.go
+++ server/middleware/auth.go
@@ -25,7 +25,7 @@ func GetAuthToken(id uuid.UUID) (string, error) {
 	claims := jwt.MapClaims{}
 	claims["authorized"] = true
 	claims["user_id"] = id
-	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &claims)
 	authToken, err := token.SignedString(tokenSecret)
 
