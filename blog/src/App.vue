7a68de46e911c95b4d2cf9050bc82fd924d9094b
hyperxpizza <wojtek.frackowski@gmail.com>
2020-12-08 19:49:43 +0100

    update

---
+++ blog/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Navigation />
+    <Blog />
+  </div>
+</template>
+
+<script>
+import Navigation from './components/Navigation.vue';
+import Blog from './components/Blog.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Navigation,
+    Blog,
+  },
+}
+</script>
---
+++ blog/src/components/Blog.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+         <section v-for="post in posts" v-bind:key="post">
+            <h2>{{ post.title }}</h2>
+            <hr>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'blog',
+    data() {
+        return{
+            posts: [],
+        }
+    },
+    mounted(){
+      this.getPosts()
+    },
+    methods: {
+        async getPosts(){
+        try {
+            const response = await fetch('http://localhost:8888/posts')
+            const data = await response.json()
+            this.posts = data
+        } catch (error){
+            console.error(error)
+      }
+    } 
+  },
+}
+</script>
